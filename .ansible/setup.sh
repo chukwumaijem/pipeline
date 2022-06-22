b066e726db706d187d6a18b08506a76996dc1fd4
Chukwuma Zikora <chukwuma.zikora@gmail.com>
2022-06-23 00:06:14 +0100

    remove private key prompt

--- .ansible/setup.sh
+++ .ansible/setup.sh
@@ -10,4 +10,4 @@ aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress
   --filters "Name=tag:Project,Values=udacity" \
   --output text >> $fileName
 
-ansible-playbook main-remote.yml -i inventory.txt --private-key AnsibleTest.pem
+ansible-playbook main-remote.yml -i inventory.txt
