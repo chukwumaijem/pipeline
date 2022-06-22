060a9d0c04cfae0015a39ea8cfeb7b08fa768092
Chukwuma Zikora <chukwuma.zikora@gmail.com>
2022-06-22 23:58:12 +0100

    pause

--- .ansible/setup.sh
+++ .ansible/setup.sh
@@ -10,4 +10,4 @@ aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress
   --filters "Name=tag:Project,Values=udacity" \
   --output text >> $fileName
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook main-remote.yml -i inventory.txt --private-key AnsibleTest.pem
+ansible-playbook main-remote.yml -i inventory.txt --private-key AnsibleTest.pem
