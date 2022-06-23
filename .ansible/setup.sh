fe6af1ba0ebb4e5775620472414983a3fa046a05
Chukwuma Zikora <chukwuma.zikora@gmail.com>
2022-06-23 07:10:52 +0100

    add ansible config

--- .ansible/setup.sh
+++ .ansible/setup.sh
@@ -10,4 +10,11 @@ aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress
   --filters "Name=tag:Project,Values=udacity" \
   --output text >> $fileName
 
+# Create ansible config file
+configFile = ".ansible.cfg"
+touch $configFile
+
+echo [defaults] > $configFile
+echo "host_key_checking = false" >> $configFile
+
 ansible-playbook main-remote.yml -i inventory.txt
